e62547699a0a9fb0cda704de6187405400e97ad7
Sock, the dev <sockthedev@gmail.com>
2023-06-08 18:10:22 +0800

    Cleans up neovim, removes gitsigns - too obtrusive

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,19 +1,4 @@
 return {
-  -- visual hints on mode
-  -- {
-  --   "mvllow/modes.nvim",
-  --   config = function()
-  --     require("modes").setup({
-  --       colors = {
-  --         copy = "#f5c359",
-  --         delete = "#c75c6a",
-  --         insert = "#78ccc5",
-  --         visual = "#9745be",
-  --       },
-  --     })
-  --   end,
-  -- },
-
   -- icons
   {
     "kyazdani42/nvim-web-devicons",
