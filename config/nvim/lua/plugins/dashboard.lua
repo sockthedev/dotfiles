8fd0c8d9ade5e8ec5d17bee17a72dc0ca500ef6a
Sock, the dev <sockthedev@gmail.com>
2024-03-25 10:14:43 +0800

    Updates dashboard

--- config/nvim/lua/plugins/dashboard.lua
+++ config/nvim/lua/plugins/dashboard.lua
@@ -13,14 +13,13 @@ return {
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button('f', ' ' .. ' Find file', ':Telescope find_files <CR>'),
-      dashboard.button('n', ' ' .. ' New file', ':ene <BAR> startinsert <CR>'),
-      dashboard.button('r', ' ' .. ' Recent files', ':Telescope oldfiles <CR>'),
-      dashboard.button('g', ' ' .. ' Find text', ':Telescope live_grep <CR>'),
-      dashboard.button('c', ' ' .. ' Config', ':e $MYVIMRC <CR>'),
-      dashboard.button('s', '勒' .. ' Restore Session', [[:lua require("persistence").load() <cr>]]),
-      dashboard.button('l', '鈴' .. ' Lazy', ':Lazy<CR>'),
-      dashboard.button('q', ' ' .. ' Quit', ':qa<CR>'),
+      dashboard.button('f', ' Find file', ':Telescope find_files <CR>'),
+      dashboard.button('n', ' New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('r', ' Recent files', ':Telescope oldfiles <CR>'),
+      dashboard.button('s', ' Restore Session', [[:lua require("persistence").load() <cr>]]),
+      dashboard.button('l', ' Lazy', ':Lazy<CR>'),
+      dashboard.button('m', ' Mason', ':Mason<CR>'),
+      dashboard.button('q', ' Quit', ':qa<CR>'),
     }
     for _, button in ipairs(dashboard.section.buttons.val) do
       button.opts.hl = 'AlphaButtons'
