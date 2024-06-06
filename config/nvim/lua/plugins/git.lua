93cdd0c81b1a94bbd8d506efea571e95168bacd9
Sock, the dev <sockthedev@gmail.com>
2024-06-06 10:28:29 +0800

    Fix fugitive in new tab keymap

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -15,7 +15,12 @@ return {
   {
     'tpope/vim-fugitive',
     config = function()
-      vim.keymap.set('n', '<leader>gf', '<cmd>tab +Git<cr>', { noremap = true, silent = true, desc = 'Open Fugitive' })
+      vim.keymap.set(
+        'n',
+        '<leader>gf',
+        '<cmd>tab Git<cr>',
+        { noremap = true, silent = true, desc = 'Open Fugitive in Tab' }
+      )
     end,
   },
 }
